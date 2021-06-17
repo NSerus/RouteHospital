7527600b9872e3dd3db8bc0f8b242f2176211363
NSerus <nunoworkingserus@gmail.com>
2021-06-17 17:55:07 +0100

    program working, just misses eliminating last services

--- main.py
+++ main.py
@@ -54,58 +54,97 @@ def distanceWtest(name, volume, dataMatrix):
         for j in range(0, len(name)-1):  # Gets the list of candidates with calculations
             candidateList.append(j)
 
-            w.append(volume[j] + 2*distancesIColumn[j])  # TODO: Learn how the calculation works  DONE
+            w.append(volume[j] + 2*distancesIColumn[j])
         print(w, "\n")
-        w=np.sort(w)  # TODO: order CandidateList by less to more  DONE?
+        bubbleSort(candidateList,w)
         print(w, "\n")
+
+        for j in range(1, len(route)):   #removes services in route from the list
+           candidateList.splice(j, 1)
+
+        j = candidateList[0]  # selects first of candidateList
+
         fullcar = True
 
 
-def distance(name, volume, dataMatrix):
+def bubbleSort(id, value):   #bubbleSorter - got it here https://www.geeksforgeeks.org/bubble-sort/
+    n = len(id)
 
-    solutions = []  #all routes are saved here??
-    totaldist = 0  #total distance of route
-    totalVolume= 0 #total volume of route
-    fullcar = False  #if car is full
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+
+        # Last i elements are already
+        #  in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to
+            # n-i-1. Swap if the element
+            # found is greater than the
+            # next element
+            if value[j] > value[j + 1]:
+                value[j], value[j + 1] = value[j + 1], value[j]
+                id[j], id[j + 1] = id[j + 1], id[j]
+                swapped = True
+
+        # IF no two elements were swapped
+        # by inner loop, then break
+        if swapped == False:
+            break
+
+def distance(name, volume, dataMatrix):
+    solutions = []  # all routes are saved here??
+    totalDist = 0  # total distance of route
+    totalVolume = 0  # total volume of route
+    aux=0      #service counter
 
     i = randint(1, len(name))  # gets random starting point
-    distancesIColumn = np.array(dataMatrix[name[i]])
+    distancesIColumn = np.array(dataMatrix[i])
+    distancesIColumn = np.delete(distancesIColumn, 0)
+    distancesIColumn = np.delete(distancesIColumn, i - 1)  # There is probably a better way to do this but i removed the name and the null value
+    volume = np.delete(volume, i - 1)
 
-    route = [name[i]] #adding the name of starting point
+    route = [name[i]]  # adding the name of starting point
 
     print("Program starts at the ", route, " service\n\n")
 
-    while (fullcar == False):
-        candidateList = []        #candidates for next route entry
-        w = []
-        for j in range(1,len(name)+1):  #Gets the list of candidates with calculations
-            candidateList.append(j)
-            w[j] = volume[j] + 2*distancesIColumn[j]#TODO: Learn how the calculation works  DONE?
+    while(aux< len(name)):
+        fullcar = False  # if car is full
+        while (fullcar == False):
+            candidateList = []  # candidates for next route entry
+            w = []
 
-        for j in range(1, route):   #removes services in route from the list
-            candidateList.splice(j, 1)
+            for j in range(0, len(name)-1):  # Gets the list of candidates with calculations
+                candidateList.append(j)
+                w.append(volume[j] + 2*distancesIColumn[j])  #calculation
 
-        np.sort(candidateList) #TODO: order CandidateList by less to more  DONE?
-        j = candidateList[0]  #selects first of candidateList
+            for j in range(1, len(route)):   #removes services in route from the list
+                candidateList.remove(j)
 
-        if (totalVolume + volume[j] < 4):  # verifies if volume + new volume doesnt overcumbers the car
-            route.append(j)  #adds new service to route
-            totalVolume += volume[j]   #adds new volume to total volume of route
-            totaldist += distancesIColumn[j]#TODO: add Distance to total distance DONE?
+            bubbleSort(candidateList, w)
 
-            i = j   #j is new i for next service in route
-            distancesIColumn = np.array(dataMatrix[name[i]])
+            j = candidateList[0]  #selects first of candidateList
 
-        else:
-            fullcar = true
-            solutions.append(route)
-            print(route, totalVolume, totaldist)
-            route = []
+            if (totalVolume + volume[j] < 4):  # verifies if volume + new volume doesnt overcumbers the car
+                route.append(name[j])  #adds new service to route
+                totalVolume += volume[j]   #adds new volume to total volume of route
+                totalDist += distancesIColumn[j]#adding distance to total distance
+
+                i = j   #j is new i for next service in route
+
+            else:
+                fullcar = True
+                solutions.append(route)
+                print(route, "\n" , totalVolume, totalDist, "\n")
+                aux+=len(route)
+                route = []
+                totalVolume=0
+                totalDist=0
 
 
 
 
 
 #constructive(dados)
-distanceWtest(GServiceName, GVolumeCar, dist_matrix)
+distance(GServiceName, GVolumeCar, dist_matrix)
 
